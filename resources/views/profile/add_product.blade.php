f357af1a616a4844e43ba803bcc9a00802a02661
bazarti-1 <hr.ahmadi6722@gmail.com>
2020-09-28 17:00:53 +0330

    ahmadi : 990705 16:59 Home & Add product

--- resources/views/profile/add_product.blade.php
+++ resources/views/profile/add_product.blade.php
@@ -4,7 +4,14 @@
     <title>بازار تهاتر ایرانیان | ثمین تخفیف</title>
 @endsection
 
+@section('extra_css')
+    {{--    <link rel="stylesheet" href="{{url('/css/cropper.css')}}">--}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.css"/>
+@endsection
+
+
 @section("content")
+
     <div class="container" style="margin-top: 80px">
         <div class="row">
             @include("profile.sidebar")
@@ -36,12 +43,15 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text font-12">نام کالا</span>
                                         </div>
-                                        <input id="product_name" name="name" type="text" class="form-control" value="{{ old("name") }}">
+                                        <input id="product_name" name="name" type="text" class="form-control"
+                                               value="{{ old("name") }}">
                                     </div>
                                 </div>
 
                                 <div class="col-12">
-                                    <textarea name="desc" id="product_desc" cols="30" rows="7" class="form-control font-13 mt-2" placeholder="توضیحات">{{ old("desc") }}</textarea>
+                                <textarea name="desc" id="product_desc" cols="30" rows="7"
+                                          class="form-control font-13 mt-2"
+                                          placeholder="توضیحات">{{ old("desc") }}</textarea>
                                 </div>
 
 
@@ -50,7 +60,8 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text font-12">قیمت (ریال)</span>
                                         </div>
-                                        <input type="number" id="product_price" name="price" class="form-control" value="{{ old("price") }}">
+                                        <input type="number" id="product_price" name="price" class="form-control"
+                                               value="{{ old("price") }}">
                                     </div>
                                 </div>
 
@@ -59,7 +70,8 @@
                                         <div class="input-group-prepend">
                                             <span class="input-group-text font-12">موبایل</span>
                                         </div>
-                                        <input type="number" id="product_mobile" name="mobile" class="form-control" value="{{ old("mobile") ?? Auth::user()->mobile }}">
+                                        <input type="number" id="product_mobile" name="mobile" class="form-control"
+                                               value="{{ old("mobile") ?? Auth::user()->mobile }}">
                                     </div>
                                 </div>
 
@@ -76,7 +88,6 @@
                                     </div>
                                 </div>
 
-
                                 <div class="col-12 col-lg-4">
                                     <div class="input-group mt-2">
                                         <div class="input-group-prepend">
@@ -84,7 +95,8 @@
                                         </div>
                                         <select name="quantity" id="product_discount" class="form-control">
                                             @for($i=1;$i<=500;$i++)
-                                                <option @if(old("quantity") == $i) selected @endif value="{{ $i }}">{{ $i }}</option>
+                                                <option @if(old("quantity") == $i) selected
+                                                        @endif value="{{ $i }}">{{ $i }}</option>
                                             @endfor
                                         </select>
                                     </div>
@@ -96,8 +108,12 @@
                                             <span class="input-group-text font-12">نوع کالا</span>
                                         </div>
                                         <select class="form-control" name="stock" id="product_stock">
-                                            <option @if(old("stock") != null && old("stock") == 1) selected @endif value="1">نو</option>
-                                            <option @if(old("stock") != null && old("stock") == 0) selected @endif value="0">کارکرده</option>
+                                            <option @if(old("stock") != null && old("stock") == 1) selected
+                                                    @endif value="1">نو
+                                            </option>
+                                            <option @if(old("stock") != null && old("stock") == 0) selected
+                                                    @endif value="0">کارکرده
+                                            </option>
                                         </select>
                                     </div>
                                 </div>
@@ -106,13 +122,12 @@
                                     <div class="input-group mt-2">
                                         <div class="custom-file">
                                             <input type="file" class="custom-file-input" name="file" id="file">
-                                            <label class="custom-file-label text-left" for="inputGroupFile01">+ افزودن عکس</label>
+                                            <label class="custom-file-label text-left" for="inputGroupFile01">+ افزودن
+                                                عکس</label>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
-
 
                             <div class="row">
                                 <div class="col-12 text-left">
@@ -129,43 +144,170 @@
                             <h2 class="mt-5">محصولات اخیر شما</h2>
                             <hr class="my-2">
                         </div>
-                            @foreach($product as $item)
-                                <div class="col-12 col-lg-6">
-                                    <div class="card shadow mt-3">
-                                        <div class="row no-gutters">
-                                            <div class="col-md-4 d-flex align-self-center">
-                                                @if(is_null($item->image))
-                                                    <img src="{{ url('/images/about.jpg') }}" class="card-img" alt="No Image">
-                                                @else
-                                                    <img src="{{ Storage::disk('vms')->url($item['image']) }}" class="card-img" alt="...">
-                                                @endif
+                        @foreach($product as $item)
+                            <div class="col-12 col-lg-6">
+                                <div class="card shadow mt-3">
+                                    <div class="row no-gutters">
+                                        <div class="col-md-4 d-flex align-self-center">
+                                            @if(is_null($item->image))
+                                                <img src="{{ url('/images/about.jpg') }}" class="card-img"
+                                                     alt="No Image">
+                                            @else
+                                                <img src="{{ Storage::disk('vms')->url($item['image']) }}"
+                                                     class="card-img"
+                                                     alt="...">
+                                            @endif
+                                        </div>
+                                        <div class="col-md-8">
+                                            <div class="card-body">
+                                                <h5 class="card-title mb-1 font-15 nowrap">@if($item->stock == 1) <span
+                                                        class="badge badge-secondary font-weight-normal">نو</span>  @else
+                                                        <span
+                                                            class="badge badge-secondary font-weight-normal">کارکرده</span>  @endif
+                                                    | {{ $item->product_name }}</h5>
+                                                0 <p class="card-text mb-1"><span
+                                                        class="text-muted font-12">قیمت:</span>
+                                                    <span class="font-18">{{ number_format($item->price) }}</span> |
+                                                    <span
+                                                        class="badge badge-danger font-14 font-weight-normal">%{{$item->discount}} تخفیف </span>
+                                                </p>
+                                                <p class="card-text"><span
+                                                        class="text-muted font-12">موجودی:</span> {{$item->quantity}}
+                                                    عدد |
+                                                    <span
+                                                        class="text-muted font-12">تاریخ:</span><span>{{ \Morilog\Jalali\Jalalian::forge($item->created_at)->format("Y/m/d") }}</span>
+                                                </p>
                                             </div>
-                                            <div class="col-md-8">
-                                                <div class="card-body">
-                                                    <h5 class="card-title mb-1 font-15 nowrap">@if($item->stock == 1) <span class="badge badge-secondary font-weight-normal">نو</span>  @else <span class="badge badge-secondary font-weight-normal">کارکرده</span>  @endif | {{ $item->product_name }}</h5>
-                                                    <p class="card-text mb-1 nowrap">{{ mb_strimwidth($item->product_desc, 0, 30, "...") }}</p>
-                                                    <p class="card-text mb-1"><span class="text-muted font-12">قیمت:</span> <span class="font-18">{{ number_format($item->price) }}</span> | <span class="badge badge-danger font-14 font-weight-normal">%{{$item->discount}} تخفیف </span></p>
-                                                    <p class="card-text"><span class="text-muted font-12">موجودی:</span> {{$item->quantity}} عدد | <span class="text-muted font-12">تاریخ:</span><span>{{ \Morilog\Jalali\Jalalian::forge($item->created_at)->format("Y/m/d") }}</span></p>
-                                                </div>
-                                                <div class="card-footer">
-                                                    <a href="{{route('delete_product_action' ,$item->id)}}" class="text-danger pl-3">حذف</a>
-                                                    <a href="{{route('edit_product' ,$item->id)}}" class="text-warning pl-3">ویرایش</a>
-                                                    <a href="{{route('view_product_single' ,$item->id)}}" class="text-success pl-3">مشاهده</a>
-                                                </div>
+                                            <div class="card-footer">
+                                                <a href="{{route('delete_product_action' ,$item->id)}}"
+                                                   class="text-danger pl-3">حذف</a>
+                                                <a href="{{route('edit_product' ,$item->id)}}"
+                                                   class="text-warning pl-3">ویرایش</a>
+                                                <a href="{{route('view_product_single' ,$item->id)}}"
+                                                   class="text-success pl-3">مشاهده</a>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            @endforeach
-                        </div>
+                            </div>
+                        @endforeach
+                    </div>
                 @endif
             </div>
         </div>
     </div>
 
 
+
+    {{--    CROPPER.JS--}}
+
+    <div class="container">
+        {{--        <h1>Laravel Cropper Js - Crop Image Before Upload - Tutsmake.com</h1>--}}
+        <input type="file" name="image" class="image">
+    </div>
+    <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    {{--                    <h5 class="modal-title" id="modalLabel">Laravel Cropper Js - Crop Image Before Upload - Tutsmake.com</h5>--}}
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="img-container">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <img id="image" src="https://avatars0.githubusercontent.com/u/3456749">
+                            </div>
+                            <div class="col-md-4">
+                                <div class="preview"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-primary" id="crop">Crop</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{--    CROPPER.JS--}}
+
 @endsection
 
 @section('extra_js')
+
     <script src="{{url('/js/app_jquery.js')}}"></script>
+    {{--    <script src="{{url('/js/jquery-cropper.js')}}"></script>--}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.6/cropper.js"></script>
+
+    {{--    CROPPER>JS--}}
+
+    <script>
+        var $modal = $('#modal');
+        var image = document.getElementById('image');
+        var cropper;
+        $("body").on("change", ".image", function (e) {
+            var files = e.target.files;
+            var done = function (url) {
+                image.src = url;
+                $modal.modal('show');
+            };
+            var reader;
+            var file;
+            var url;
+            if (files.length > 0) {
+                file = files[0];
+                if (URL) {
+                    done(URL.createObjectURL(file));
+                } else if (FileReader) {
+                    reader = new FileReader();
+                    reader.onload = function (e) {
+                        done(reader.result);
+                    };
+                    reader.readAsDataURL(file);
+                }
+            }
+        });
+        $modal.on('shown.bs.modal', function () {
+            cropper = new Cropper(image, {
+                aspectRatio: 1,
+                viewMode: 3,
+                preview: '.preview'
+            });
+        }).on('hidden.bs.modal', function () {
+            cropper.destroy();
+            cropper = null;
+        });
+        $("#crop").click(function () {
+            canvas = cropper.getCroppedCanvas({
+                width: 160,
+                height: 160,
+            });
+            canvas.toBlob(function (blob) {
+                url = URL.createObjectURL(blob);
+                var reader = new FileReader();
+                reader.readAsDataURL(blob);
+                reader.onloadend = function () {
+                    var base64data = reader.result;
+                    $.ajax({
+                        type: "POST",
+                        dataType: "json",
+                        url: "crop-image-upload",
+                        data: {'_token': $('meta[name="_token"]').attr('content'), 'image': base64data},
+                        success: function (data) {
+                            console.log(data);
+                            $modal.modal('hide');
+                            alert("Crop image successfully uploaded");
+                        }
+                    });
+                }
+            });
+        })
+    </script>
+
+    {{--    CROPPER>JS--}}
 @endsection
