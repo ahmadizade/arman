0889f4577fc67cbd5f1d26ef3dc45b1f8c79ee13
bazarti-1 <hr.ahmadi6722@gmail.com>
2020-10-01 13:27:56 +0330

    ahmadi : 990710 13:27 ADMIN

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,6 +15,7 @@
     <link href="{{url('admin/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
           rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
 
 
     <!-- Custom styles for this template-->
@@ -927,6 +928,8 @@
 <!-- Page level custom scripts -->
 <script src="{{url('/admin/js/demo/chart-area-demo.js')}}"></script>
 <script src="{{url('/admin/js/demo/chart-area-demo.js')}}"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
 
 </body>
 
