26228f5cd472ca302acfbdfd44f107ac29e9cf25
bazarti-1 <hr.ahmadi6722@gmail.com>
2020-10-12 17:03:49 +0330

    Datatable : 990718 17:07 ADMIN

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,13 +6,20 @@
 
 @section("content")
 
-    <div class="container mt-3">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <img src="/images/bg/contact_banner_dsk.jpg" class="img-fluid shadow" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container">
         <div class="row">
             <div class="col-12 col-lg-12">
                 <div class="card shadow mt-3">
                     <div class="card-header p-3">
                         <h3 class="mb-0 font-14 float-right"><span
-                                class="far fa-address-card font-weight-bold pt-1 text-primary"></span> تماس با ما </h3>
+                                class="far fa-address-card font-weight-bold pt-1 text-primary"></span> فرم ثبت درخواست و پشتیبانی </h3>
                     </div>
                     <div class="card-body p-3">
                         @if ($errors->any())
@@ -31,7 +38,7 @@
                             <div class="col-12 col-lg-12">
 
                                 <div class="row">
-                                    <div class="col-12 col-lg-3 mb-2 text-center border-bottom-1 pb-2">
+                                    <div class="col-12 col-lg-3 mb-2 text-center bg-success border-bottom-1 pb-2">
                                         <img src="/images/icon/download (1).png" class="bti-icon img-fluid" title="Bazarti-link">
                                         <span>ثمین سایت ها</span>
                                     </div>
@@ -58,10 +65,10 @@
                                 <div class="row">
                                     <div class="input-group">
                                         <div class="col-12 col-lg-6 mt-2">
-                                            <input id="name" name="name" class="form-control" value="">
+                                            <input id="name" name="name" class="form-control" value="نام و نام خانوادگی">
                                         </div>
                                         <div class="col-12 col-lg-6 mt-2">
-                                            <input id="mobile" name="mobile" class="form-control" value="">
+                                            <input id="mobile" name="mobile" class="form-control" value="شماره تماس">
                                         </div>
                                     </div>
                                 </div>
@@ -72,6 +79,7 @@
                                         </div>
                                     </div>
                                 </div>
+                                <button type="submit" name="contact_btn" id=",contact-btn" class="btn btn-primary btn-sm">ارسال درخواست</button>
                             </form>
                         </div>
                     </div>
