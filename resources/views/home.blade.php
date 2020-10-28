a00dca9161a87b427aa17880acf2d043b191986c
mehdirajolani <mehdirajolani@gmail.com>
2020-10-28 16:58:59 +0330

    Rajolani 1399/08/07 16:59 Home Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,27 @@
     <div class="container-fluid" style="border-top:2px solid #8b8788">
         <div class="row">
             <div class="col-12">
-                <img src="/images/home/slider.jpg" class="img-fluid" alt="">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block w-100" src="images/home/categories.jpg" alt="First slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="images/home/no-money.jpg" alt="Second slide">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="images/home/bazarti.jpg" alt="Third slide">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -258,7 +278,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="owl-container">
-                        <div class="owl-carousel owl-theme owl shadow-box green">
+                        <div class="owl-carousel owl-theme owl">
                             @foreach($randomProduct as $product)
                                 <a href="{{ route("shop_product_single",["id" => $product->id]) }}">
                                     <div class="slider-desc text-center overflow-hidden">
