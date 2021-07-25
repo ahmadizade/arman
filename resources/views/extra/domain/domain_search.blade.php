775c6abbbce78c83279e5de8ef0baaf9917af2d4
hr.ahmadi6722@gmail.com <Abcd/1234>
2021-07-25 21:55:24 +0430

    Merge branch 'work' of https://github.com/ahmadizade/cioce_new_face into ahmadi
    # Conflicts:
    #	.idea/workspace.xml

--- resources/views/extra/domain/domain_search.blade.php
+++ resources/views/extra/domain/domain_search.blade.php
@@ -30,36 +30,56 @@
         </div>
         <div class="container main-container">
             <div class="row">
-                <div class="col-12">
                     <div class="page info-page-cats dt-sl dt-sn pt-3 pb-2">
                         <div class="row">
-                            <div class="col-12 pr-4 mb-3">
-                                <div class="section-title title-wide no-title-wide-before mb-1 no-after-title-wide">
+                            <div class="col-12 col-md-6 col-lg-6">
+                                <div class="section-title title-wide no-title-wide-before mb-1 no-after-title-wide mt-5">
                                     <img src="/img/faq/question.svg" width="30" alt="">
                                     <h2 class="font-weight-bold">چطور میتونم صاحب سایت و ارتباطش با سایت های دیگرو پیدا کنم؟</h2>
                                     <p>فقط کافیه که آدرس سایت خودتو تو کادر بالا بنویسی و دکمه جستجو رو بزنی</p>
                                 </div>
+                                <div class="content-faq-question">
+                                    <p class="text-justify">همچنین در این بخش می توانید ایمیل های اصلی سایت جستجو شده را ببینید و متوجه شوید که این ایمیل ها در چه سایت هایی عضو هستند، درواقع شما متوجه ارتباطات آن سایت با دیگر وب سایت ها و کمپانی ها می شوید، اینجوری تو هم می تونی از اون سایت ها و روابط اونا برای رشد کسب و کار خودت استفاده کنی.</p>
+                                </div>
                             </div>
-                            <div class="col-12">
-                                <div class="row">
-                                    <div class="col-12 col-md-6 col-lg-6">
-                                        <div class="content-faq-question">
-                                            <p class="text-justify">همچنین در این بخش می توانید ایمیل های اصلی سایت جستجو شده را ببینبد و متوجه شوید که این ایمیل ها در چه سایت هایی عضو هستند، درواقع شما متوجه ارتباطات آن سایت با دیگر وب سایت ها و کمپانی ها می شوید، اینجوری تو هم می تونی از اون سایت ها و روابط اونا برای رشد کسب و کار خودت استفاده کنی.</p>
+                            <div class="col-12 col-md-6 col-lg-6">
+                                <img src="/img/banner/wat-is-een-domeinnaam.png">
+                            </div>
+                        </div>
 
-                                        </div>
-                                    </div>
-
-                                    <div class="col-12 col-md-6 col-lg-6">
-                                        <div class="content-faq-question">
-                                            <img src="/img/theme/domain-names-2.jpg">
-
-                                        </div>
-                                    </div>
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-lg-6">
+                                <img src="/img/banner/afloopt.png">
+                            </div>
+                            <div class="col-12 col-md-6 col-lg-6">
+                                <div class="section-title title-wide no-title-wide-before mb-1 no-after-title-wide mt-5">
+                                    <img src="/img/faq/question.svg" width="30" alt="">
+                                    <h2 class="font-weight-bold">بدست آوردن نام و نام خانوادگی صاحب وب سایت؟</h2>
+                                    <p>بدست آوردن ارتباط سایت ها با یکدیگر</p>
+                                </div>
+                                <div class="content-faq-question">
+                                    <p class="text-justify">برای بدست آوردن نام و نام خانوادگی صاحب وب سایت شما باید عضو سایت شوید و امتیاز شما در وب سایت ما بیش از 400 امتیاز باشد، درضمن زمانی که صاحب آن سایت اطلاعات شخصی خود را در بانک دامنه های ملی ثبت کرده باشد ما قادر به دریافت اطلاعات وی خواهیم بود، در غیر اینصورت می توانیم اطلاعاتی مثل ایمیل های اصلی آن وب سایت و اینکه با آن ایمیل ها در چه سایت هایی عضو شده اند را برای شما بدست آورده و نمایش دهیم.</p>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="row">
+                            <div class="col-12 col-md-6 col-lg-6">
+                                <div class="section-title title-wide no-title-wide-before mb-1 no-after-title-wide mt-5">
+                                    <img src="/img/faq/question.svg" width="30" alt="">
+                                    <h2 class="font-weight-bold">پیدا کردن نام و نام خانوادگی صاحب ایمیل؟</h2>
+                                    <p>فقط کافیه که به صفحه جستجوی صاحب ایمیل رفته و ایمیل را وارد کنید</p>
+                                </div>
+                                <div class="content-faq-question">
+                                    <p class="text-justify">ما اطلاعاتی نظیر نام و نام خانوادگی صاحب ایمیل، ارتباطات وی در دنیای مجازی و... را به شما نمایش خواهیم داد، البته که برای روعیت نام و نام خانوادگی با عضو سایت ما باشید و امتیاز شما در سایت ما بالای 400 امتیاز باشد، این بدین معناست که شما همراه همره ما و ما نیز خدمتگذار شما هستیم.</p>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-md-6 col-lg-6">
+                                <img src="/img/banner/registratie.png">
+                            </div>
+                        </div>
                     </div>
-                </div>
             </div>
         </div>
     </main>
